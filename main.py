ef8a276286dba003e631b9fbe0efe44894b1ea15
JagrutiSakhare <37575384+JagrutiSakhare@users.noreply.github.com>
2023-03-23 12:30:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def hello_gcs(event, context):
+    import anchor
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+    anchor.move_file(event['bucket'], event['name']);
